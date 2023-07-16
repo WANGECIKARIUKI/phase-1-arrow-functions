df79ed165121da72fc1d92c5982c89c5a1b0692f
LUCY <wangecikariu.luc@gmail.com>
2023-07-16 22:52:56 +0300

    Arrows Lab

---
+++ index.js
@@ -0,0 +1,16 @@
+function divide(x / y) {
+    return x/y;
+}
+
+const x =2000;
+const y =100;
+const divide = x / y;
+console.log(divide(x, y));
+
+const square = a => a** a;
+
+let b = 2;
+let multiplication = (2, 2) => 2 * 2;
+
+const add = (c,d) => c + d;
+
