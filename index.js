71ddeeea9fb16a6718106427046a8854a75a8c9f
LUCY <wangecikariu.luc@gmail.com>
2023-08-02 19:53:31 +0300

    Arrow Function

--- index.js
+++ index.js
@@ -1,16 +1,8 @@
-function divide(x / y) {
-    return x/y;
+function divide (x, y) {
+    x = 2000;
+    y = 100;
+    return 2000 / 100;
 }
-
-const x =2000;
-const y =100;
-const divide = x / y;
-console.log(divide(x, y));
-
 const square = a => a** a;
 
-let b = 2;
-let multiplication = (2, 2) => 2 * 2;
-
 const add = (c,d) => c + d;
-
